14aafeed9798093120f1e39ec7749da1c1a61ac7
Allan Glasier <allan@phoenixdesign.ca>
2020-09-18 20:44:27 -0400

    Added images, gallery loop working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  const images = data.allFile.nodes
+
+  return images.map((image, index) => {
+    return <Img fluid={image.childImageSharp.fluid} key={index} />
+  })
 }
+
+export const query = graphql`
+  {
+    allFile {
+      nodes {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+`
