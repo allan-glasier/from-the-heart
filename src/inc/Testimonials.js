2ebefcd9b325f029f8bb01fcb7fa74169a0ea680
Allan Glasier <allan@phoenixdesign.ca>
2020-10-29 13:11:03 -0400

    added testimonial carousel

--- src/inc/Testimonials.js
+++ src/inc/Testimonials.js
@@ -1,36 +1,210 @@
 import React from "react"
+import Carousel from "react-elastic-carousel"
 
 const Testimonials = () => {
   return (
     <section className="section container pb-5">
       <div className="testimonials pt-5 pb-5">
         <h2 className="text-center">What People are Saying About Us</h2>
-        <blockquote class="blockquote text-center px-5 pt-5">
-          <p>
-            I wish we could put into words how grateful we are to the both of
-            you. Not only did Celebrity Music coordinate our rehearsal dinner;
-            they also insured our wedding day was flawless. It did not matter
-            what was asked Joe & Sharon were on top of everything!! Our guests
-            raved about our amazing coordinator/DJ/MC duo from the moment they
-            first heard Joe over the mic.
-          </p>
-          <p>
-            Sharon and Joe were constantly insuring everything was okay and did
-            anything and everything asked to make sure we had the most amazing
-            day. This included bringing drinks to us where ever we happened to
-            be, helping with 'bridely' duties and just being overall amazing.
-          </p>
-          <p className="mb-0">
-            We have no doubt in our minds that our day would not have been the
-            same without the two of you there. Our guests had a blast and the
-            dance floor was full the entire night. Thank you does not seem to
-            suffice for all that you guys have done to make sure our wedding day
-            was out of a fairy tale.
-          </p>
-          <footer class="blockquote-footer">
-            Allan & Sara - <cite title="Source Title">June 27th, 2015</cite>
-          </footer>
-        </blockquote>
+        <Carousel itemsToShow={1} enableAutoPlay={true} autoPlaySpeed={5000}>
+          <blockquote class="blockquote text-center px-5 pt-5">
+            <p>
+              I wish we could put into words how grateful we are to the both of
+              you. Not only did Celebrity Music coordinate our rehearsal dinner;
+              they also insured our wedding day was flawless. It did not matter
+              what was asked Joe & Sharon were on top of everything!! Our guests
+              raved about our amazing coordinator/DJ/MC duo from the moment they
+              first heard Joe over the mic.
+            </p>
+            <p>
+              Sharon and Joe were constantly insuring everything was okay and
+              did anything and everything asked to make sure we had the most
+              amazing day. This included bringing drinks to us where ever we
+              happened to be, helping with 'bridely' duties and just being
+              overall amazing.
+            </p>
+            <p className="mb-0">
+              We have no doubt in our minds that our day would not have been the
+              same without the two of you there. Our guests had a blast and the
+              dance floor was full the entire night. Thank you does not seem to
+              suffice for all that you guys have done to make sure our wedding
+              day was out of a fairy tale.
+            </p>
+            <footer class="blockquote-footer">
+              Allan & Sara - <cite title="Source Title">June 27th, 2015</cite>
+            </footer>
+          </blockquote>
+          <blockquote class="blockquote text-center px-5 pt-5">
+            <p>
+              Wonderful Personality of both Joe and Sharron!. I always laughed
+              during the one on one meetings.
+            </p>
+            <p>
+              They have been doing this for a long time, they have met with
+              different people. They know how to manage people.
+            </p>
+            <p className="mb-0">
+              When you get their services utilize them to the fullest extent! I
+              guarantee you will not regret the decision! Use them! They are
+              there to help you! 100% value for what they offer.
+            </p>
+            <footer class="blockquote-footer">
+              Far Han - <cite title="Source Title">June 18th, 2020</cite>
+            </footer>
+          </blockquote>
+          <blockquote class="blockquote text-center px-5 pt-5">
+            <p className="mb-0">
+              This power couple just gave us one of the best days of our life.
+              very organized, charismatic, always there to help with any and
+              everything. they treated my bride and our families with such
+              respect and I couldn't have asked for more. Joe and Sharon, much
+              love and blessings from Mr. and Mrs. Darbison.
+            </p>
+            <footer class="blockquote-footer">
+              Kris Darbison -{" "}
+              <cite title="Source Title">February 16th, 2020</cite>
+            </footer>
+          </blockquote>
+          <blockquote class="blockquote text-center px-5 pt-5">
+            <p className="mb-0">
+              Joe and Sharon were absolutely amazing to work with! They had
+              wonderful tips at our planning meetings, ran our rehearsal, and
+              kept everything on the day of the wedding running smoothly. They
+              were incredibly organized and professional, and were prepared for
+              any little thing that could have happened. During the reception
+              they kept our dance floor so full, we had a blast! They picked
+              songs that kept everyone - little kids to grandparents- dancing
+              and singing along! I would 100% recommend Joe and Sharon,
+              incredible to work with! Thank you for making my wedding memorable
+              and amazing!
+            </p>
+            <footer class="blockquote-footer">
+              Victoria Elizabeth -{" "}
+              <cite title="Source Title">July 31st, 2019</cite>
+            </footer>
+          </blockquote>
+          <blockquote class="blockquote text-center px-5 pt-5">
+            <p className="mb-0">
+              Joe and Sharon are absolutely amazing. They went absolutely above
+              and beyond what a regular dj/photographer would do. I would
+              recommend them to anybody getting married. They're both absolutely
+              amazing and I don't know what we would have done without them.
+              Absolutely amazing photographers and DJs, had the crowd going the
+              whole event and took beautiful photos
+            </p>
+            <footer class="blockquote-footer">
+              Tim Huff - <cite title="Source Title">June 25th, 2019</cite>
+            </footer>
+          </blockquote>
+          <blockquote class="blockquote text-center px-5 pt-5">
+            <p className="mb-0">
+              We were SO happy with our wedding last year, this couple did a
+              fantastic job with our bi-cultural wedding- making sure both sides
+              of our families music was incorporated keeping EVERYONE dancing
+              all night. We would highly recommend that you enlist their
+              services FIRST - before anyone else. They have been working in the
+              industry in Windsor long enough to give you the best guidance on
+              all of your other vendors. They spend time with you, they get to
+              know you and they care about how your day will go. They are
+              honest, professional, and worth every penny.
+            </p>
+            <footer class="blockquote-footer">
+              Yasmin Ghandehari -{" "}
+              <cite title="Source Title">November 16th, 2018</cite>
+            </footer>
+          </blockquote>
+          <blockquote class="blockquote text-center px-5 pt-5">
+            <p className="mb-0">
+              We chose to hire Joe and Sharon for our wedding and they were
+              incredible! They work so well together as a team and make sure
+              that everything is taken care of so that you can focus on the
+              memorable moments. Joe and Sharon went above and beyond to make
+              sure that we were completely catered to all night long! They are
+              very accommodating and will work with you to make sure that
+              everything goes off without a hitch. Sharon made sure that every
+              small detail was attended to and taken care of and Joe had the
+              dance floor packed all night long! To anyone that is getting
+              married I would highly recommend that you meet with Joe and Sharon
+              before hiring anyone else.
+            </p>
+            <footer class="blockquote-footer">
+              Katie Lafontaine -{" "}
+              <cite title="Source Title">September 24th, 2018</cite>
+            </footer>
+          </blockquote>
+          <blockquote class="blockquote text-center px-5 pt-5">
+            <p className="mb-0">
+              My wife and I just got married this past Sunday. From the moment
+              we met Joe and Sharon we knew that we had a winning combination on
+              our hands. They were the most pleasant and funny people we could
+              have wished to have on our team while we were planning our
+              wedding. They were soooo much more than a DJ and videographer.
+              They were there every step of the way ensuring that our special
+              day was as perfect as could be. Joe has an amazing sense of
+              humour( a perfect m c) while Sharon has an impeccable attention to
+              detail and was there making sure that we were ok throughout our
+              day. The attention and professionalism that Joe and Sharon
+              provided us was absolutely amazing. The music was awesome and our
+              guests danced and danced all night long. We were so glad that we
+              chose Celebrity Music to join us on our special day!! We would
+              DEFINITELY recommend Joe and Sharon for your special functions.
+              You won't be disappointed!!!
+            </p>
+            <footer class="blockquote-footer">
+              Joe Caruana - <cite title="Source Title">October 12th, 2017</cite>
+            </footer>
+          </blockquote>
+          <blockquote class="blockquote text-center px-5 pt-5">
+            <p className="mb-0">
+              I've had the opportunity to work with Joe for a few weddings now.
+              I must say everyone needs to hire Celebrity Music. Joe is honest
+              and gets things done! His sense of humour is always entertaining
+              the morning of the wedding! Being a makeup artist in the industry
+              I look forward to working with Celebrity Music. They definitely
+              wear many hats in the process leading to your wedding day. They
+              are organized, efficient and will most certainly take care of you!
+              I cannot say enough great things about them. I highly recommend
+              them!
+            </p>
+            <footer class="blockquote-footer">
+              Anita Deanne - <cite title="Source Title">July 25th, 2017</cite>
+            </footer>
+          </blockquote>
+          <blockquote class="blockquote text-center px-5 pt-5">
+            <p>
+              Joe and Sharon have changed what it means to offer wedding DJ
+              services. You might as well fire your wedding coordinator and hire
+              these two to run the whole show.{" "}
+            </p>
+            <p>
+              Joe is an excellent DJ, able to command the room with his booming
+              radio voice and knows what it takes to get people dancing all
+              night.
+            </p>
+            <p>
+              Sharon is the hidden gem in this operation, she watches every
+              detail throughout the night and has a solution to literally any
+              situation that may come up. She provided some key necessities that
+              my bride and I didn't even think we could need, she reported to us
+              on anything time-sensitive that was happening throughout the night
+              and silently handled various little challenges that a wedding
+              inevitably has. She even made sure everyone got onto the busses at
+              the end of the night!{" "}
+            </p>
+            <p className="mb-0">
+              The duo are absolute professionals and I would recommend them to
+              anyone getting married. Wedding DJ industry beware - the
+              competition is fierce in these two, and the road is long if you
+              want to provide the value that these two bring. Plus they're super
+              nice and always focused on making your wedding what YOU want it to
+              be. Thank you Sharon and Joe, A+++ service.
+            </p>
+            <footer class="blockquote-footer">
+              Kevin C. Whelan -{" "}
+              <cite title="Source Title">December 20th, 2016</cite>
+            </footer>
+          </blockquote>
+        </Carousel>
       </div>
     </section>
   )
