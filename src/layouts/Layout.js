9f97d115c152e89f846c6913d979c04d226e378c
Allan Glasier <allan@phoenixdesign.ca>
2020-10-31 00:58:21 -0400

    removed old comments

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,6 +1,5 @@
 import React from "react"
 import "../styles/bootstrap.min.css"
-// import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "../inc/Navbar"
 import Contact from "../inc/Contact"
 import Footer from "../inc/Footer"
