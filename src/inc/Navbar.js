5cf11d17a6161ae9d9bc59947223c9f1957a6d0c
Allan Glasier <allan@phoenixdesign.ca>
2020-10-31 00:57:02 -0400

    added mobile menu

--- src/inc/Navbar.js
+++ src/inc/Navbar.js
@@ -1,67 +1,124 @@
 import { Link } from "gatsby"
 import React from "react"
+import { Nav, Navbar } from "react-bootstrap"
 
-import "../styles/bootstrap.min.css"
-
-export default function Navbar() {
+export default function Navigation() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarColor03"
-        aria-controls="navbarColor03"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="mobile-logo">
+    <Navbar expand="lg" bg="light" fixed="top">
+      <Navbar.Toggle aria-controls="navbarResponsive" />
+      <Navbar.Brand href="/" className="mobile-logo">
         <img src={"/fthLogo.png"} alt="" />
-        <img src={"/celebrityLogo.png"} alt="" />
-      </div>
-
-      <div className="collapse navbar-collapse" id="navbarColor03">
-        <ul className="navbar-nav mr-auto mx-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">
+        <img className="ml-3" src={"/celebrityLogo.png"} alt="" />
+      </Navbar.Brand>
+      <Navbar.Collapse id="navbarResponsive">
+        <Nav as="ul" className="mx-auto">
+          <Nav.Item as="li">
+            <Link to="/about" className="nav-link" activeClassName="active">
               About
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/gallery">
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="/gallery" className="nav-link" activeClassName="active">
               Gallery
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/videography">
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link
+              to="/videography"
+              className="nav-link"
+              activeClassName="active"
+            >
               Videography
             </Link>
-          </li>
-          <Link className="navbar-brand" to="/">
+          </Nav.Item>
+          <Navbar.Brand href="/" className="large-logo">
             <img src={"/fthLogo.png"} alt="" />
-          </Link>
-          <Link className="navbar-brand" to="/">
-            <img src={"/celebrityLogo.png"} alt="" />
-          </Link>
-          <li className="nav-item">
-            <Link className="nav-link" to="/dj-choreography">
-              DJ / choreography
+            <img className="ml-5" src={"/celebrityLogo.png"} alt="" />
+          </Navbar.Brand>
+          <Nav.Item as="li">
+            <Link
+              to="/dj-choreography"
+              className="nav-link"
+              activeClassName="active"
+            >
+              DJ/Choreograpy
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/blog">
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="/blog" className="nav-link" activeClassName="active">
               Blog
             </Link>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#contact">
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="#contact" className="nav-link" activeClassName="active">
               Contact
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+            </Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
+
+// export default function Navbar() {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+//       <button
+//         className="navbar-toggler"
+//         type="button"
+//         data-toggle="collapse"
+//         data-target="#navbarColor03"
+//         aria-controls="navbarColor03"
+//         aria-expanded="false"
+//         aria-label="Toggle navigation"
+//       >
+//         <span className="navbar-toggler-icon"></span>
+//       </button>
+//       <div className="mobile-logo">
+//         <img src={"/fthLogo.png"} alt="" />
+//         <img src={"/celebrityLogo.png"} alt="" />
+//       </div>
+
+//       <div className="collapse navbar-collapse" id="navbarColor03">
+//         <ul className="navbar-nav mr-auto mx-auto">
+//           <li className="nav-item">
+//             <Link className="nav-link" to="/about">
+//               About
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link className="nav-link" to="/gallery">
+//               Gallery
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link className="nav-link" to="/videography">
+//               Videography
+//             </Link>
+//           </li>
+//           <Link className="navbar-brand" to="/">
+//             <img src={"/fthLogo.png"} alt="" />
+//           </Link>
+//           <Link className="navbar-brand" to="/">
+//             <img src={"/celebrityLogo.png"} alt="" />
+//           </Link>
+//           <li className="nav-item">
+//             <Link className="nav-link" to="/dj-choreography">
+//               DJ / choreography
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link className="nav-link" to="/blog">
+//               Blog
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <a className="nav-link" href="#contact">
+//               Contact
+//             </a>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   )
+// }
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "../styles/bootstrap.min.css"
+// import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "../inc/Navbar"
 import Contact from "../inc/Contact"
 import Footer from "../inc/Footer"
